5d7a835299ed04e82406d39e7d51951a828b17d2
William9923 <williamong1400@gmail.com>
2021-09-09 08:34:29 +0700

    test(activation): added unitest for each activation function

--- src/activation/activation.py
+++ src/activation/activation.py
@@ -1,5 +1,5 @@
 import numpy as np
-import numpy.typing as npt
+from typing import Union
 
 class Activation:
     """
@@ -15,7 +15,7 @@ class Activation:
     """
 
     @staticmethod
-    def relu(x: npt.DTypeLike[np.float64] | npt.NDArray[np.float64], deriv: bool = False) -> npt.DTypeLike[np.float64] | npt.NDArray[np.float64] :
+    def relu(x: Union[float, np.ndarray], deriv: bool = False) -> Union[float, np.ndarray] :
         """
         [Flow-Function]
             1. Check if using derivative option
@@ -36,7 +36,7 @@ class Activation:
         return np.maximum(0, x)
 
     @staticmethod
-    def sigmoid(x: npt.DTypeLike[np.float64] | npt.NDArray[np.float64], deriv:bool = False) -> npt.DTypeLike[np.float64] | npt.NDArray[np.float64]:
+    def sigmoid(x: Union[float, np.ndarray], deriv:bool = False) -> Union[float, np.ndarray] :
         """
         [Flow-Function]
             1. Check if using derivative option
@@ -52,30 +52,26 @@ class Activation:
             output (float64) | Array(float64)
         """
         if deriv:
-            return x * (1 - x)
+            return Activation.sigmoid(x) * (1 - Activation.sigmoid(x))
         return 1 / (1 + np.exp(-x)) 
 
 
     @staticmethod
-    def softmax(x: npt.NDArray[np.float64], deriv:bool = False) ->  npt.NDArray[np.float64]:
+    def softmax(x: np.ndarray, deriv:bool = False) -> np.ndarray :
         """
         [Flow-Function]
             1. Check if using derivative option
             2. If derivative, use :
-                f'(x)=0 => if x < 0
-                f'(x)=1 => if x >= 0
+                pass ??
             3. If not (normal calculation), use :
-                f(x) = max(0, x)
+                f(x) = exp(xi) / Σexp(x)
                 
         [Params]
             x Array(float64) -> 1 sequence only
 
         [Return]
-            output Array(float64) 
+            output Array(float64) -> Probability of all output (sum(output) == 1.0)
         """
         if deriv:
             pass
         return np.exp(x) / np.sum(np.exp(x), axis=0)
-
-if __name__ == "__main__" :
-    pass 
