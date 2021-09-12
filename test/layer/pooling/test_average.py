d91e571708006b2fc75d396f4ec3eb775d07550c
Felicia Gojali <gojalif@gmail.com>
2021-09-12 11:23:31 +0700

    feat: avg pooling layer

---
+++ test/layer/pooling/test_average.py
@@ -0,0 +1,177 @@
+import pytest
+import numpy as np
+from src.layer import AveragePooling2D
+
+matrix = np.array(
+    [
+        np.array([1, 2, 3, 2, 3]),
+        np.array([8, 3, 7, 4, 9]),
+        np.array([9, 1, 4, 7, 2]),
+        np.array([2, 6, 5, 3, 9]),
+    ]
+)
+
+matrix2 = np.array(
+    [
+        np.array([1, 2, 3, 2, 3]),
+        np.array([8, 3, 7, 4, 9]),
+        np.array([9, 1, 4, 7, 2]),
+    ]
+)
+
+matrix3 = np.array(
+    [
+        np.array([1, 2, 3, 2, 3, 4]),
+        np.array([8, 3, 7, 4, 9, 4]),
+        np.array([9, 1, 4, 7, 2, 10]),
+        np.array([2, 6, 5, 3, 9, 1]),
+        np.array([8, 3, 7, 4, 9, 3]),
+        np.array([9, 1, 4, 7, 2, 12]),
+        np.array([2, 6, 5, 3, 9, 10]),
+    ]
+)
+
+matrix4 = np.array(
+    [
+        np.array([1, 2, 3, 2, 3, 4 , 2 , 3 , 4 , 1]),
+        np.array([8, 3, 7, 4, 9, 4 , 2 , 3 , 4 , 1]),
+        np.array([9, 1, 4, 7, 2, 10, 3 , 10, 30, 3]),
+        np.array([2, 6, 5, 3, 9, 1 , 4 , 6 , 1 , 3]),
+        np.array([8, 3, 7, 4, 9, 3 , 23, 11, 23, 33]),
+        np.array([9, 1, 4, 7, 2, 12, 2 , 3 , 4 , 5]),
+        np.array([2, 6, 5, 3, 9, 10, 1 , 1 , 1 , 1]),
+    ]
+)
+
+
+def createBatch(data):
+    batch = np.array(
+        [
+            data.copy(),
+            data.copy(),
+            data.copy(),
+            data.copy(),
+        ]
+    )
+
+    return batch
+
+
+def stackData(matrix):
+    data = np.stack(
+        [
+            matrix.copy(),
+            matrix.copy(),
+            matrix.copy(),
+        ],
+        axis=-1,
+    )
+
+    return data
+
+
+def data(matrix):
+    data = stackData(matrix)
+    batch = createBatch(data)
+    return batch
+
+
+@pytest.mark.parametrize(
+    "name, batch, params, expected_output",
+    [
+        (
+            "Test 1 - Pool data 1",
+            data(matrix),
+            ((3, 3), (1, 1)),
+            np.array(
+                [
+                    np.stack(
+                        [
+                            np.array(
+                                [
+                                    np.array([4.22, 3.67, 4.56]),
+                                    np.array([5.0, 4.44, 5.56]),
+                                ],
+                            )
+                            for _ in range(3)
+                        ],
+                        axis=-1
+                    )
+                    for _ in range(4)
+                ]
+            ),
+        ),
+        (
+            "Test 2 - Pool data 2",
+            data(matrix2),
+            ((2, 2), (1, 1)),
+            np.array(
+                [
+                    np.stack(
+                        [
+                            np.array(
+                                [
+                                    np.array([3.5, 3.75, 4, 4.5]),
+                                    np.array([5.25, 3.75, 5.5, 5.5]),
+                                ],
+                            )
+                            for _ in range(3)
+                        ],
+                        axis=-1
+                    )
+                    for _ in range(4)
+                ]
+            ),
+        ),
+        (
+            "Test 3 - Pool data 3",
+            data(matrix3),
+            ((3, 3), (2, 2)),
+            np.array(
+                [
+                    np.stack(
+                        [
+                            np.array(
+                                [
+                                    np.array([4.22, 4.56]),
+                                    np.array([5.0, 5.56]),
+                                    np.array([5.0, 5.56]),
+                                ],
+                            )
+                            for _ in range(3)
+                        ],
+                        axis=-1
+                    )
+                    for _ in range(4)
+                ]
+            ),
+        ),
+        (
+            "Test 4 - Pool data 4",
+            data(matrix4),
+            ((3, 3), (2, 2)),
+            np.array(
+                [
+                    np.stack(
+                        [
+                            np.array(
+                                [
+                                    np.array([4.22, 4.56, 4.33, 6.78]),
+                                    np.array([5.0, 5.56, 7.11, 12.33]),
+                                    np.array([5.0, 5.56, 7.89, 7.67]),
+                                ],
+                            )
+                            for _ in range(3)
+                        ],
+                        axis=-1
+                    )
+                    for _ in range(4)
+                ]
+            ),
+        ),
+    ],
+)
+def test_max_pooling_propagate(name, batch, params, expected_output):
+    layer = AveragePooling2D(*params)
+    out = layer.forward_propagation(batch)
+    np.testing.assert_equal(out, expected_output)
