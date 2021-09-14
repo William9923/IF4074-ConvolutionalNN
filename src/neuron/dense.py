a03b5a34d3177fa89fa7aaf80cc0391c746f832a
Samuel <samnap11@gmail.com>
2021-09-14 13:39:49 +0700

    feat(dense): add dense neuron

--- src/neuron/dense.py
+++ src/neuron/dense.py
@@ -1,3 +1,7 @@
+import numpy as np
+from src.utility import denseComputation
+
+
 class NeuronDense:
     """
     [Description]
@@ -5,12 +9,67 @@ class NeuronDense:
         This class contains dot product computation
 
     [Attributes]
+        _weight_shape (Tuple(row, col))
+        weight (Array(row, col))
 
     [Method]
-
-    TODO:
-        - Create convolution computation
+        build
+        compute
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, weight_shape):
+        """
+        [Params]
+            weight_shape (Tuple(row, col)) -> Shape for the weight matrix (excluding bias) where col, row >= 1
+        """
+        self._weight_shape = weight_shape
+
+    def build(self):
+        """
+        Build weight matrix based on weight_shape value
+
+        [Flow-Method]
+            1. Create bias matrix (assume it's always 1)
+            2. Create weight matrix by randomization
+            3. Concatenate bias and weight matrix
+            4. Assign weight matrix to weight attribute
+
+        [Example]
+            bias = [[1. 1. 1.]]
+            weight = [[0.5509331  0.51313228 0.66194263]
+                      [0.03711788 0.46608808 0.6175996 ]
+                      [0.92120823 0.01023161 0.4123873 ]]
+
+            weight_and_bias = [[1.         1.         1.        ]
+                               [0.5509331  0.51313228 0.66194263]
+                               [0.03711788 0.46608808 0.6175996 ]
+                               [0.92120823 0.01023161 0.4123873 ]]
+        """
+        row, col = self._weight_shape
+
+        bias = np.ones((1, col))
+
+        weight = np.array([np.random.rand(*(col,)) for _ in range(row)])
+
+        weight_and_bias = np.concatenate((bias, weight))
+
+        self.weight = weight_and_bias
+
+    def compute(self, batch):
+        """
+        [Params]
+            batch (Array(batch, m, 1)) -> A batch of data in which each data is in the shape of m x 1 (m,), including bias
+
+        [Return]
+            out (Array(batch, k, 1)) -> A batch of output in which each output is in the shape of k x 1 (k,) where k is the column count of weight
+        """
+        out = []
+        for data in batch:  # data (Array(m, 1))
+            product = [1]  # bias for output
+            computation = denseComputation(data, self.weight)
+
+            product.extend(computation)
+            out.append(product)
+
+        out = np.array(out)
+        return out
