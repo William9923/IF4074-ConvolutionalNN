25c7d77e3a19c6cefd5c8f89af82aaeae14362ad
William9923 <williamong1400@gmail.com>
2021-09-15 22:43:39 +0700

    chore(detector): comment correction

--- src/layer/detector/sigmoid.py
+++ src/layer/detector/sigmoid.py
@@ -7,7 +7,7 @@ from src.layer.detector.detector import Detector
 class Sigmoid(Detector):
     """
     [Description]
-        Perform ReLU function as activation for output from previous layer
+        Perform Sigmoid function as activation for output from previous layer
         - Used after the Conv Layer as detection phase in CNN.
         - Used after the Dense Layer as activation func in Dense.
     """
