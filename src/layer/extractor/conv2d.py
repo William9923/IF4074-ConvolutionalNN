[fix] backprop for convolution from 5d to 4d

src/layer/extractor/conv2d.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from src.layer.interface import Layer
 from src.neuron import NeuronConv2D
+from src.optimizer import SGD
 from src.utility import (
     pad2D,
     calc_convoluted_shape,
@@ -156,8 +157,8 @@ class Conv2D(Layer):
         output = [neuron.compute(self.input) for neuron in self._neurons]
 
         # Save output for every neuron
-        self.output = np.array(output)
         output = np.stack(output, axis=-1)
+        self.output = np.array(output)
         return output
 
     def backward_propagation(self, errors):
@@ -166,18 +167,16 @@ class Conv2D(Layer):
 
 
         [Params]
-            errors (batch, neurons, row, col, channels) -> row and col based on kernel_shape, 
+            errors (batch, row, col, channels) -> row and col based on kernel_shape, 
                 channels based on input_shape, 
                 neurons based on how many filters used in this layer
 
         [Return]
-            propagated_error (batch, neurons, row, col, channels)
+            propagated_error (batch, row, col, channels)
         """
         dEdIns = []
-        for neuron, error in zip(self._neurons, np.rollaxis(errors, 1)): # error (Array(batch, row, col, channels))
-            dEdIns.append(neuron.update_weights(error))
+        for neuron, error in zip(self._neurons, np.rollaxis(errors, 3)): # error (Array(batch, row, col))
+            dEdIns.append(neuron.update_weights(SGD(), error))
         
-        dEdIns = np.array(dEdIns)
-        n_neuron, n_batch, n_row, n_col, n_channel = dEdIns.shape
-        dEdIns = dEdIns.reshape(n_batch, n_neuron, n_row, n_col, n_channel)
+        dEdIns = np.sum(dEdIns, axis=0)
         return dEdIns

src/neuron/conv2d.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from src.utility import convolve2D, conv2d_derivative
+from src.utility import convolve2D, dilate, pad2D
 
 
 class NeuronConv2D:
@@ -84,12 +84,42 @@ class NeuronConv2D:
             )  # convoluted (Array(row, col, channel))
             out.append(convoluted)
 
-        out = np.array(out)
+        out = np.array(out)  # out (Array(batch, row, col, channel))
+        self.raw_output = out
         out = np.sum(out, axis=-1)  # out (Array(batch, row, col))
+        self.output_without_bias = out
         out += self._bias
 
         self.output = out
         return out
+
+    def _dEdW(self, error, input):
+        """
+        [Params]
+            error (Array(row, col))
+            input (Array(row, col))
+        
+        [Return]
+            dEdW (Array(row, col))
+        """
+        dilated_error = dilate(error, self._stride)
+        dEdW = convolve2D(input, dilated_error)
+        return dEdW
+    
+    def _dEdIn(self, error, kernel):
+        """
+        [Params]
+            error (Array(row, col))
+            kernel (Array(row, col))
+        
+        [Return]
+            dEdIn (Array(row, col))
+        """
+        rotated_kernel = np.rot90(np.rot90(kernel))
+        dilated_error = dilate(error, self._stride)
+        padding = (self._kernel_shape[0]-1, self._kernel_shape[0]-1, self._kernel_shape[1]-1, self._kernel_shape[1]-1)
+        dEdIn = convolve2D(pad2D(dilated_error, pad=padding), rotated_kernel)
+        return dEdIn
 
     def update_weights(self, opt, batch_error):
         """
@@ -104,39 +134,37 @@ class NeuronConv2D:
 
         [Params]
             opt (Optimizer) -> optimizer params from sequential
-            batch_error (Array(batch, row, col, channel)) -> row and col based on _kernels_shape, and channel based on input_shape
+            batch_error (Array(batch, row, col)) -> row and col based on _kernels_shape
         """
-        dEdWs, dEdIns = [], []
-
-        for input, error in zip(
+        dEdWs_batch, dEdIns_batch = [], []
+        for input, matrix_error in zip(
             self.input, batch_error
-        ):  # data (Array(row, col, channel)) error (Array(row, col, channel))
+        ):  # input (Array(row, col, channel)) error (Array(row, col))
+            dEdWs, dEdIns = [], []
+            for matrix_input, matrix_kernel in zip(
+                np.rollaxis(input, 2), np.rollaxis(self._kernels, 2)
+            ):  # matrix_input (Array(row, col)) matrix_kernel (Array(row, col))
+                dEdW = self._dEdW(matrix_error, matrix_input)
+                dEdIn = self._dEdIn(matrix_error, matrix_kernel)
+                # print(matrix_error.shape)
+                # print(matrix_kernel.shape)
+                # print(dEdIn.shape)
+                # print()
+                dEdWs.append(dEdW)
+                dEdIns.append(dEdIn)
 
-            gradient_channels = []
-            local_error_channels = []
-            for matrix_input, matrix_error, matrix_kernel in zip(
-                np.rollaxis(input, 2),
-                np.rollaxis(error, 2),
-                np.rollaxis(self._kernels, 2),
-            ):
-                dEdW, dEdIn = conv2d_derivative(
-                    matrix_error, matrix_kernel, matrix_input, self._stride
-                )
-                gradient_channels.append(dEdW)
-                local_error_channels.append(dEdIn)
+            dEdWs = np.stack(dEdWs, axis=-1)
+            dEdIns = np.stack(dEdIns, axis=-1)
+            dEdWs_batch.append(dEdWs)
+            dEdIns_batch.append(dEdIns)
 
-            gradient_channels = np.stack(gradient_channels, axis=-1)
-            local_error_channels = np.stack(local_error_channels, axis=-1)
-            dEdWs.append(gradient_channels)
-            dEdIns.append(local_error_channels)
-
-        dEdWs = np.array(dEdWs)  # gradients (Array(batch, row, col, channel))
-        dEdIns = np.array(dEdIns)
+        dEdWs_batch = np.array(dEdWs_batch)  # dEdWs_batch (Array(batch, row, col, channel))
+        dEdIns_batch = np.array(dEdIns_batch)
 
         # Updating weights
-        gradients = np.sum(dEdWs, axis=0)
+        gradients = np.sum(dEdWs_batch, axis=0)
         for i, (kernel2d, gradient2d) in enumerate(
             zip(np.rollaxis(self._kernels, 2), np.rollaxis(gradients, 2))
         ):
             self._kernels[:, :, i] = opt.update_matrix(kernel2d, gradient2d)
-        return dEdIns
+        return dEdIns_batch