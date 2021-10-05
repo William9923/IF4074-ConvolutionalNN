d25bbfcc3355b3f0bbd4da38444447b73f9109b0
Samuel <samnap11@gmail.com>
2021-10-05 12:36:04 +0700

    test: add new tests for backward propagation of dense layer

--- test/layer/extractor/test_layer_dense.py
+++ test/layer/extractor/test_layer_dense.py
@@ -18,3 +18,24 @@ def test_dense_layer(name, params, batch, expected_output_shape):
     dense_layer = Dense(*params)
     out = dense_layer.forward_propagation(batch)
     assert out.shape == expected_output_shape, f"Wrong output shape"
+
+
+@pytest.mark.parametrize(
+    "name, batch, errors, layer_params, expected_shape",
+    [
+        (
+            "Test 1 - Test Shape",
+            np.random.rand(2, 4),
+            np.random.rand(2, 2),
+            (2, 4),
+            (2, 4),
+        )
+    ],
+)
+def test_backward_prop(name, batch, errors, layer_params, expected_shape):
+    layer = Dense(*layer_params)
+    layer.forward_propagation(batch)
+    new_err = layer.backward_propagation(errors)
+    assert (
+        new_err.shape == expected_shape
+    ), f"Expected {expected_shape} got {new_err.shape}"
